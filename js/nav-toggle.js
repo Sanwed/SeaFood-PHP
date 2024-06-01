262862f4fda22b66e69796ded6287743f41ee452
Alexandr Kozyukov <kozyuckov.alex@gmail.com>
2024-06-01 19:26:44 +0400

    Delete js directory

--- js/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-const body = document.body;
-const btn = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
-
-btn.addEventListener('click', () => {
-  nav.classList.toggle('active');
-  body.classList.toggle('no-scroll');
-})
