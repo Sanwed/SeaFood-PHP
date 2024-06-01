13b77f938d41916b5132a7687256c3df72ea7379
Alexandr Kozyukov <kozyuckov.alex@gmail.com>
2024-06-01 19:26:35 +0400

    Delete functions directory

--- functions/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-
-$_SESSION['auth']  = null;
-$_SESSION['login'] = null;
-header('Location: ../pages/index.php');
--- functions/remove_from_card.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-
-$_SESSION['basket'] = array_diff($_SESSION['basket'], [$_GET['id']]);
-header('Location: ../pages/basket.php');
